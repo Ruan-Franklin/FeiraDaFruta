9f533e3cd0b0ac647052a00690c48214775c46a8
ruan-franklin <ruanfranklinmengo@gmail.com>
2024-10-27 08:39:14 -0300

    :sparkles: feat: adicionando view para index

--- feiradafruta/settings.py
+++ feiradafruta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-489k=-jt=55^#!0x4-i+@!x7=s=ntskk@q)btdjy$(+8s*uryv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ruanfknatal.pythonanywhere.com']
+ALLOWED_HOSTS = ['ruanfknatal.pythonanywhere.com', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'feiradafruta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
