350ba190e7a4fe4858dedb13266b49bdc1fb17e8
Ruan-Franklin <ruanfranklinmengo@gmail.com>
2024-10-27 19:17:23 +0000

    :sparkles: feat: adicionando telas finais

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, HistoricoView, CuriosidadeView, ReferenciaView, CaracteristicaView, RegraView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'), 
+    path('', IndexView.as_view(), name='index'),
+    path('historico/', HistoricoView.as_view(), name='historico'),
+    path('curiosidades/', CuriosidadeView.as_view(), name='curiosidade'),
+    path('caracteristicas/', CaracteristicaView.as_view(), name='caracteristica'),
+    path('referencias/', ReferenciaView.as_view(), name='referencia'),
+    path('regras/', RegraView.as_view(), name='regra'),
+
+
+
+
+
+
+
+
 ]
--- app/views.py
+++ app/views.py
@@ -2,3 +2,13 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+class HistoricoView(TemplateView):
+    template_name = "historico.html"
+class CuriosidadeView(TemplateView):
+    template_name = "curiosidade.html"
+class RegraView(TemplateView):
+    template_name = "regra.html"
+class ReferenciaView(TemplateView):
+    template_name = "referencia.html"
+class CaracteristicaView(TemplateView):
+    template_name = "caracteristica.html"
